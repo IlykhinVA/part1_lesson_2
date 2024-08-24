1fa1b58e6991bb3959f9e43c68be26d52cacdc52
IlykhinVA <priest49175@gmail.com>
2024-08-24 10:16:47 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 Home_Works=12
 Time_Work=1.5
-Route_Name='Pyton,'
+Route_Name='Python,'
 Average_Time=Time_Work/Home_Works
 print("Курс:",Route_Name,'всего задач:',(str(Home_Works)+','),' затрачено часов:',(str(Time_Work)+','),'среднее время выполнения',Average_Time,'часа')
